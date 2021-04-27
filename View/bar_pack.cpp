9cad4a93b3f687cfc9f3013e7badf2c181d1b787
George Krovsh <gosha.krovsh@gmail.com>
2021-04-27 20:27:32 +0300

    Fix resize bug and some smaller ones
    Добавлен класс BarPack, который позволяет сделать правильное поведение при
    изменении размеров окна.

---
+++ View/bar_pack.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gosha_krovsh on 26.04.21.
+//
+
+#include "bar_pack.h"
---
+++ View/bar_pack.h
@@ -0,0 +1,12 @@
+//
+// Created by gosha_krovsh on 26.04.21.
+//
+
+#ifndef BAR_PACK_H
+#define BAR_PACK_H
+
+class BarPack {
+
+};
+
+#endif //BAR_PACK_H
