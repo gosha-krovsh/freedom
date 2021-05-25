0ca6b4efbc2290893e4606e42c5f473edf6d1d6e
George Krovsh <gosha.krovsh@gmail.com>
2021-05-26 02:49:34 +0300

    Fix

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QStyleFactory>
 
 #include "Controller/controller.h"
 
@@ -6,6 +7,28 @@ int main(int argc, char* argv[]) {
   QApplication a(argc, argv);
 
   auto controller = std::make_unique<Controller>();
+
+  qApp->setStyle(QStyleFactory::create("Fusion"));
+
+  QPalette darkPalette;
+  darkPalette.setColor(QPalette::Window, QColor(53,53,53));
+  darkPalette.setColor(QPalette::WindowText, Qt::white);
+  darkPalette.setColor(QPalette::Base, QColor(25,25,25));
+  darkPalette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+  darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+  darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+  darkPalette.setColor(QPalette::Text, Qt::white);
+  darkPalette.setColor(QPalette::Button, QColor(53,53,53));
+  darkPalette.setColor(QPalette::ButtonText, Qt::white);
+  darkPalette.setColor(QPalette::BrightText, Qt::red);
+  darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+
+  darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+  darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+  qApp->setPalette(darkPalette);
+
+  qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
 
   return QApplication::exec();
 }
